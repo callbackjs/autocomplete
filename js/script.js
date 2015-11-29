43d7022b149abf50b3e5502fd0dcc3f7dacdab86
Karthik Viswanathan <karthik.ksv@gmail.com>
2015-11-29 19:48:08 +0530

    Switch to two space indentation.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 (function(window, document, undefined) {
-    // numeric identifiers for enter, down, and up arrow keys
-    var ENTER_KEY_CODE = 13;
-    var DOWN_ARROW_KEY_CODE = 40;
-    var UP_ARROW_KEY_CODE = 38;
+  // numeric identifiers for enter, down, and up arrow keys
+  var ENTER_KEY_CODE = 13;
+  var DOWN_ARROW_KEY_CODE = 40;
+  var UP_ARROW_KEY_CODE = 38;
 
-    // TODO: autocomplete code
+  // TODO: autocomplete code
 
 })(this, this.document);

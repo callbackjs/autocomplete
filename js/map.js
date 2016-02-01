235b591d7080ac67301c524cc0c655d03640d05a
Vivek Nair <vnair611@stanford.edu>
2016-02-01 10:09:02 -0800

    Remove the IIFE in the maps

--- js/map.js
+++ js/map.js
@@ -1,52 +1,50 @@
-(function(window, document, undefined) {
-  // initial zoom value to pass to Google Maps API when generating static maps
-  var GOOGLE_MAPS_INITIAL_ZOOM = 8;
-  var searchForm = document.getElementById('search-form');
-  var map = null;
+// initial zoom value to pass to Google Maps API when generating static maps
+var GOOGLE_MAPS_INITIAL_ZOOM = 8;
+var searchForm = document.getElementById('search-form');
+var map = null;
 
-  searchForm.addEventListener('submit', function(event) {
-    // clear autocomplete suggestions if they exist
-    var suggestions = document.getElementById('suggestions');
-    if (suggestions) {
-      suggestions.innerHTML = '';
+searchForm.addEventListener('submit', function(event) {
+  // clear autocomplete suggestions if they exist
+  var suggestions = document.getElementById('suggestions');
+  if (suggestions) {
+    suggestions.innerHTML = '';
+  }
+
+  var mapExists = (map !== null);
+  event.preventDefault();
+
+  if (!mapExists) {
+    // create image element to hold map
+    map = document.createElement('img');
+    map.id = 'map';
+  }
+
+  var zoomLevel = GOOGLE_MAPS_INITIAL_ZOOM;
+  var searchTerm = search.value;
+
+  // zoom out every one second by means of asynchronous recursion
+  (function zoomOut() {
+    if (zoomLevel === 0) {
+      // we've finished zooming
+      return;
     }
 
-    var mapExists = (map !== null);
-    event.preventDefault();
+    // update source address based off zoom level
+    map.src = 'http://maps.google.com/maps/api/staticmap?center=' +
+      searchTerm + '&size=640x395&maptype=roadmap&sensor=false' +
+      '&zoom=' + zoomLevel;
 
-    if (!mapExists) {
-      // create image element to hold map
-      map = document.createElement('img');
-      map.id = 'map';
-    }
+    // decrement zoom in preparation for next recursive call
+    zoomLevel--;
 
-    var zoomLevel = GOOGLE_MAPS_INITIAL_ZOOM;
-    var searchTerm = search.value;
+    // callback in one second
+    setTimeout(zoomOut, 1000);
+  })();
 
-    // zoom out every one second by means of asynchronous recursion
-    (function zoomOut() {
-      if (zoomLevel === 0) {
-        // we've finished zooming
-        return;
-      }
+  map.alt = 'Map of ' + searchTerm;
 
-      // update source address based off zoom level
-      map.src = 'http://maps.google.com/maps/api/staticmap?center=' +
-        searchTerm + '&size=640x395&maptype=roadmap&sensor=false' +
-        '&zoom=' + zoomLevel;
-
-      // decrement zoom in preparation for next recursive call
-      zoomLevel--;
-
-      // callback in one second
-      setTimeout(zoomOut, 1000);
-    })();
-
-    map.alt = 'Map of ' + searchTerm;
-
-    if (!mapExists) {
-      // add map to DOM if it was just created
-      searchForm.parentNode.appendChild(map);
-    }
-  });
-})(this, this.document);
+  if (!mapExists) {
+    // add map to DOM if it was just created
+    searchForm.parentNode.appendChild(map);
+  }
+});
